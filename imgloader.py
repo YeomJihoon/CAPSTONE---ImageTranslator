62a078c9be872f00e4c9d76e707e88cbf0b410c2
YeomJihoon <37164092+YeomJihoon@users.noreply.github.com>
2020-11-24 21:17:02 +0900

    Add files via upload
    사이즈 조정기능 추가

--- imgloader.py
+++ imgloader.py
@@ -6,14 +6,31 @@ from PIL import Image, ImageTk
 
 loader = tkinter.Tk()
 
-def load():
-    filename = filedialog.askopenfilename(initialdir="/", title = "select file")
+def load(route):
+    filename = filedialog.askopenfilename(initialdir=route, title = "select file")
+    
     return filename
 
-name = load()
-image = ImageTk.PhotoImage(Image.open(name))
+route = "/"     # 첫 켰을 때 기본경로 값 없음
 
-label = tkinter.Label(loader, image = image)
-label.pack()
+name = load(route)
+route_split = name.split("/")
+del route_split[-1]
+route = "/".join(route_split)  # 켜진 상태에서 다시 로드할 때 전 이미지와 같은 경로에서 시작
+
+image = Image.open(name)
+
+img_hor = image.size[0]
+img_ver = image.size[1]
+if ((img_hor/1024) > (img_ver/768)):
+    rate = img_hor/1024
+else:
+    rate = img_ver/768               # 사이즈 변경을 위한 비율정하
+
+resizing = image.resize((int(img_hor/rate),int(img_ver/rate)))  # 이미지 크기 변경
+
+outimage = ImageTk.PhotoImage(resizing)
+label = tkinter.Label(loader, image = outimage, width = 1024, height = 768)
+label.pack()  # 크기 변경된 이미지를 출력
 
 loader.mainloop()
